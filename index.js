74fc091a4c90e8e601e67df49d1565966cd600ed
ADM <darkgaroto@gmail.com>
2021-09-21 20:10:13 -0300

    :full_moon: Mudanças na estrutura

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const path = require('path')
+const news = require('./routes/news')
+
 const app = express()
 
-app.set('views', path.join(__dirname, 'public/views'))
+app.set('views', path.join(__dirname, 'views'))
 app.engine('handlebars', exphbs())
 app.set('view engine', 'handlebars') // => Diz ao node que utliza-se os modelos Pug
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static('public'))
 
 app.get('/', (require, response) => {
   response.render('index', {layout: false})
 })
+
+//Rotas
+app.use('/news', news)
 
 const server = app.listen(1601, () => {
   console.log(`[⚡] O servidor está rodando na porta ${server.address().port}`)
